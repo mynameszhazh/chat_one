6ee1594e31a0ae684e52682e830728bc705e82a7
mynameszhazh <1594776259@qq.com>
2021-09-27 14:28:59 +0800

    'socket.io的基本使用'

---
+++ src/socket.io/server/index.js
@@ -0,0 +1,23 @@
+// const ios = require('socket.io')
+// import ios from 'socket.io'
+// const app = require('http').createServer(handle)
+// const fs = require('fs')
+// const path = require('path')
+// const io = ios(app, { cors: true })
+// app.listen(8081)
+// function handle (req, res) {
+//   fs.readFile(path.join(__dirname, '/index.html'), (err, data) => {
+//     if (err) {
+//       res.end(err)
+//     } else {
+//       res.end(data)
+//     }
+//   })
+// }
+// //
+// io.on('connection', client => {
+//   client.emit('event', { msg: 'data' })
+//   client.on('events', (data) => {
+//     console.log(data, '客户端连接')
+//   })
+// })
