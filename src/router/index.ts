40c6d3da160bd14d5d66edc929f521fa97970b3a
mynameszhazh <1594776259@qq.com>
2021-10-11 17:03:31 +0800

    新增一个用socket.io完成的聊天router

--- src/router/index.ts
+++ src/router/index.ts
@@ -25,6 +25,14 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Chat.vue')
+  },
+  {
+    path: '/socketChat',
+    name: 'socketChat',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/socketChat/index.vue')
   }
 ]
 
